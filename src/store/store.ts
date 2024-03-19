109d8b79d417b1c5f44d124642f864c95025b090
Ruslan Otonbaev <ruslan_otonbaev@epam.com>
2024-03-19 15:13:54 +0600

    add scripts to run deploy process in aws including cloudfront,cloudformation and bucket services

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,17 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from 'store/counterSlice';
-import cartReducer from 'store/cartSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "store/counterSlice";
+import cartReducer from "store/cartSlice";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    cart: cartReducer
+    cart: cartReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+export type AppDispatch = ReturnType<typeof store.dispatch>;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
