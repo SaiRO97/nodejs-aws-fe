bebe8f1fbcb046401db44e6789f8316237e9b7ce
Ruslan Otonbaev <ruslan_otonbaev@epam.com>
2024-03-28 22:35:21 +0600

    connect products to lambda

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,11 @@
+const BASE_URL = "https://cxb01tga8k.execute-api.us-east-1.amazonaws.com";
 
 const API_PATHS = {
-  product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: `${BASE_URL}/products`,
+  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
